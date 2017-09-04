a1a5e828d661bc29c6d06fe5fc3bba0f7f4ee4f3
wentianyang <yqqq2015@gmail.com>
2017-09-04 11:10:27 +0800

    Practice09 MatrixRotate 利用MatrixRotate进行旋转

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice09MatrixRotateView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice09MatrixRotateView.java
@@ -4,19 +4,21 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
-
 import com.hencoder.hencoderpracticedraw4.R;
 
 public class Practice09MatrixRotateView extends View {
+
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Bitmap bitmap;
     Point point1 = new Point(200, 200);
     Point point2 = new Point(600, 200);
+    Matrix mMatrix = new Matrix();
 
     public Practice09MatrixRotateView(Context context) {
         super(context);
@@ -26,7 +28,8 @@ public class Practice09MatrixRotateView extends View {
         super(context, attrs);
     }
 
-    public Practice09MatrixRotateView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public Practice09MatrixRotateView(Context context, @Nullable AttributeSet attrs,
+        int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
@@ -38,7 +41,21 @@ public class Practice09MatrixRotateView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        mMatrix.reset();
+        mMatrix
+            .postRotate(180, point1.x + bitmap.getWidth() / 2, point1.y + bitmap.getHeight() / 2);
+
+        canvas.save();
+        canvas.concat(mMatrix);
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+        mMatrix.reset();
+        mMatrix.postRotate(45, point2.x + bitmap.getWidth() / 2, point2.y + bitmap.getHeight() / 2);
+
+        canvas.save();
+        canvas.concat(mMatrix);
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
     }
 }
