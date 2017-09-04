b1e5f8d752a45cfcb74c375a295755b4cebe8172
wentianyang <yqqq2015@gmail.com>
2017-09-04 10:42:36 +0800

    Practice04 Scale 利用 canvas scale 进行缩放

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice04ScaleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice04ScaleView.java
@@ -9,10 +9,10 @@ import android.graphics.Point;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
-
 import com.hencoder.hencoderpracticedraw4.R;
 
 public class Practice04ScaleView extends View {
+
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Bitmap bitmap;
     Point point1 = new Point(200, 200);
@@ -38,7 +38,14 @@ public class Practice04ScaleView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        canvas.save();
+        canvas.scale(1.4f, 1.4f, point1.x + bitmap.getWidth() / 2, point1.y + bitmap.getHeight() / 2);
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+        canvas.save();
+        canvas.scale(0.5f, 1.5f, point2.x  + bitmap.getWidth() / 2, point2.y + bitmap.getHeight() / 2);
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
     }
 }
