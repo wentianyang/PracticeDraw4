41112487d2ec5ef73cf0521ce08801c07d3ec80c
wentianyang <yqqq2015@gmail.com>
2017-09-04 11:49:08 +0800

    Practice12 CameraRotate 利用 Camera rotate 进行旋转

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
@@ -3,20 +3,22 @@ package com.hencoder.hencoderpracticedraw4.practice;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
-
 import com.hencoder.hencoderpracticedraw4.R;
 
 public class Practice12CameraRotateFixedView extends View {
+
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Bitmap bitmap;
     Point point1 = new Point(200, 200);
     Point point2 = new Point(600, 200);
+    Camera mCamera = new Camera();
 
     public Practice12CameraRotateFixedView(Context context) {
         super(context);
@@ -26,7 +28,8 @@ public class Practice12CameraRotateFixedView extends View {
         super(context, attrs);
     }
 
-    public Practice12CameraRotateFixedView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public Practice12CameraRotateFixedView(Context context, @Nullable AttributeSet attrs,
+        int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
@@ -38,7 +41,34 @@ public class Practice12CameraRotateFixedView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        float centerX1 = point1.x + bitmap.getWidth() / 2;
+        float centerY1 = point1.y + bitmap.getHeight() / 2;
+
+        float centerX2 = point2.x + bitmap.getWidth() / 2;
+        float centerY2 = point2.y + bitmap.getHeight() / 2;
+
+        canvas.save();
+
+        mCamera.save();
+        mCamera.rotateX(30);
+        canvas.translate(centerX1, centerY1);
+        mCamera.applyToCanvas(canvas);
+        canvas.translate(-centerX1, -centerY1);
+        mCamera.restore();
+
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+        canvas.save();
+
+        mCamera.save();
+        mCamera.rotateY(30);    //旋转Camera的三维空间
+        canvas.translate(centerX2, centerY2);   //旋转之后移动回原来的位置
+        mCamera.applyToCanvas(canvas);      //把旋转投影到Canvas
+        canvas.translate(-centerX2, -centerY2);     //旋转之前把绘制内容移动到轴心（原点）
+        mCamera.restore();
+
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
     }
 }
