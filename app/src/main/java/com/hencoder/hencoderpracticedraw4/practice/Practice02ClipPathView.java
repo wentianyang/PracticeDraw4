38618534fa1bf17d293fbf898d0ff4d24c80059f
wentianyang <yqqq2015@gmail.com>
2017-09-04 10:30:55 +0800

    Practice02 ClipPath 利用Canvas clipPath 进行裁切

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice02ClipPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice02ClipPathView.java
@@ -6,18 +6,22 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Path.Direction;
+import android.graphics.Path.FillType;
 import android.graphics.Point;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
-
 import com.hencoder.hencoderpracticedraw4.R;
 
 public class Practice02ClipPathView extends View {
+
     Paint paint = new Paint();
     Bitmap bitmap;
     Point point1 = new Point(200, 200);
     Point point2 = new Point(600, 200);
+    Path path1 = new Path();
+    Path path2 = new Path();
 
     public Practice02ClipPathView(Context context) {
         super(context);
@@ -33,13 +37,24 @@ public class Practice02ClipPathView extends View {
 
     {
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
+        path1.addCircle(point1.x + 200, point1.y + 200, 150, Direction.CW);
+
+        path2.setFillType(FillType.INVERSE_WINDING);
+        path2.addCircle(point2.x + 200, point2.y + 200, 150, Direction.CW);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        canvas.save();
+        canvas.clipPath(path1);
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+        canvas.save();
+        canvas.clipPath(path2);
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
     }
 }
